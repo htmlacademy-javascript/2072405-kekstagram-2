460e66c38913b10ac6cd9d0411fc1050a34ef067
Vlx32 <129527577+Vlx32@users.noreply.github.com>
2025-08-11 13:12:03 +0300

    fix: выносит константы из кода в переменную среду

--- js/main.js
+++ js/main.js
@@ -50,6 +50,14 @@ const DESCRIPTIONS = [
 
 const PHOTOS_COUNT = 25;
 
+const LIKES_RANGE = {min: 15, max: 200};
+
+const COMMENTS_RANGE = {min: 0, max: 30};
+
+const MESSAGE_COUNT_RANGE = {min: 1, max: 2};
+
+const AVATAR_RANGE = {min: 1, max: 6};
+
 let commentIdCounter = 1;
 
 const getRandomInteger = (a, b) => {
@@ -62,7 +70,7 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createComment = () => {
-  const messageCount = getRandomInteger(1, 2);
+  const messageCount = getRandomInteger(MESSAGE_COUNT_RANGE.min, MESSAGE_COUNT_RANGE.max);
   let message = getRandomArrayElement(MESSAGES);
 
   if (messageCount === 2) {
@@ -76,21 +84,21 @@ const createComment = () => {
 
   return {
     id: commentIdCounter++,
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    avatar: `img/avatar-${getRandomInteger(AVATAR_RANGE.min, AVATAR_RANGE.max)}.svg`,
     message: message,
     name: getRandomArrayElement(NAMES)
   };
 };
 
 const createPhoto = (index) => {
-  const commentsCount = getRandomInteger(0, 30);
+  const commentsCount = getRandomInteger(COMMENTS_RANGE.min, COMMENTS_RANGE.max);
   const comments = Array.from({length: commentsCount}, createComment);
 
   return {
     id: index + 1,
     url: `photos/${index + 1}.jpg`,
     description: DESCRIPTIONS[index],
-    likes: getRandomInteger(15, 200),
+    likes: getRandomInteger(LIKES_RANGE.min, LIKES_RANGE.max),
     comments: comments
   };
 };
